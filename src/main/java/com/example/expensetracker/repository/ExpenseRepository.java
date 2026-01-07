536fd7dc5cb80940f1b0ab3d7e01b8a042348939
IT24100235 <it24100235@my.sliit.lk>
2026-01-07 10:58:48 +0530

    Progress so far

--- src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
@@ -1,4 +1,7 @@
 package com.example.expensetracker.repository;
 
-public class ExpenseRepository {
+import com.example.expensetracker.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
